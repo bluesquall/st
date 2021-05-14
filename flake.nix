b846586ab735cb08259eaec94b3f17c696b7b44e
M J Stanway <m.j.stanway@alum.mit.edu>
2021-05-13 18:46:19 -0700

    actually build 'st'
    In retrospect, it may be more maintainable to use a nix overlay to apply
    the patches I want, instead of maintaining a separate fork repository
    and adding a flake to it... but it was a decent learning experience
    anyway.

--- flake.nix
+++ flake.nix
@@ -1,14 +1,24 @@
 {
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs";
-    flake-utils.url = "github:numtide/flake-utils";
-  };
+  description = "a flake to build `st` (simple terminal) with some patches";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system};
-      in {
-        packages.hello = pkgs.hello;
-        devShell = pkgs.mkShell { buildInputs = [ pkgs.hello pkgs.cowsay ]; };
-      });
+      let pkgs = nixpkgs.legacyPackages.${system}; in
+      rec {
+        apps.st = flake-utils.lib.mkApp { drv = packages.st; };
+        defaultApp = apps.st;
+        defaultPackage = packages.st;
+        packages = flake-utils.lib.flattenTree {
+          st = pkgs.stdenv.mkDerivation {
+            name = "st";
+            buildInputs = with pkgs; [ pkg-config fontconfig ncurses freetype x11 ];
+            src = self;
+            buildPhase = "make clean all";
+            installPhase = "TERMINFO=$out/share/terminfo make install PREFIX=$out";
+          };
+        };
+      }
+    );
 }

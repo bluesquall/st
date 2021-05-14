5f6fe55a6230dbe6b4562bf9818d00604ef0cfff
M J Stanway <m.j.stanway@alum.mit.edu>
2021-05-13 17:16:16 -0700

    use flake-utils

--- flake.nix
+++ flake.nix
@@ -1,15 +1,14 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs }:
-    let pkgs = nixpkgs.legacyPackages.x86_64-linux;
-    in {
-      packages.x86_64-linux.hello = pkgs.hello;
-
-      devShell.x86_64-linux = pkgs.mkShell {
-        buildInputs = [ self.packages.x86_64-linux.hello pkgs.cowsay ];
-      };
-  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        packages.hello = pkgs.hello;
+        devShell = pkgs.mkShell { buildInputs = [ pkgs.hello pkgs.cowsay ]; };
+      });
 }

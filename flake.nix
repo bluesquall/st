cd47e455e0aa52eace6ff88955933812db31d6d4
M J Stanway <m.j.stanway@alum.mit.edu>
2021-05-13 17:04:22 -0700

    ... and again a scosche less trivial

--- flake.nix
+++ flake.nix
@@ -1,5 +1,9 @@
 {
-  outputs = { self }: {
-    foo = "bar";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+  };
+
+  outputs = { self, nixpkgs }: {
+    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
   };
 }

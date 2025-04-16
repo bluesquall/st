a90c2dce96c612a6694cec86f4513ac7ed1e2329
M Jordan Stanway <m.j.stanway@alum.mit.edu>
2025-04-15 17:23:53 -0700

    apply `scrollback-float` patch
    source: https://st.suckless.org/patches/scrollback/
    actual patch: https://st.suckless.org/patches/scrollback/st-scrollback-float-0.9.2.diff

--- st.c
+++ st.c
@@ -1090,14 +1090,14 @@ tswapscreen(void)
 void
 kscrollup(const Arg *a)
 {
-	int n = a->i;
+	float n = a->f;
 
 	if (IS_SET(MODE_ALTSCREEN))
 		return;
 
-	if (n < 0) n = (-n) * term.row;
+	if (n < 0) n = MAX((-n) * term.row, 1);
 	if (n > TSCREEN.size - term.row - TSCREEN.off) n = TSCREEN.size - term.row - TSCREEN.off;
-	while (!TLINE(-n)) --n;
+	while (!TLINE((int)-n)) --n;
 	TSCREEN.off += n;
 	selscroll(0, n);
 	tfulldirt();
@@ -1107,12 +1107,12 @@ void
 kscrolldown(const Arg *a)
 {
 
-	int n = a->i;
+	float n = a->f;
 
 	if (IS_SET(MODE_ALTSCREEN))
 		return;
 
-	if (n < 0) n = (-n) * term.row;
+	if (n < 0) n = MAX((-n) * term.row, 1);
 	if (n > TSCREEN.off) n = TSCREEN.off;
 	TSCREEN.off -= n;
 	selscroll(0, -n);
